6c1c9d17692690aa9ff847a90a0b28ae4b4e57ab
Gregory Kurtzer <gmkurtzer@gmail.com>
2020-12-06 01:11:57 -0800

    Not sure why my .gitignore is causing some files to be skipped... Have to look into that.

---
+++ internal/app/wwctl/container/pull/root.go
@@ -0,0 +1,28 @@
+package pull
+
+import "github.com/spf13/cobra"
+
+var (
+	baseCmd = &cobra.Command{
+		Use:   "pull",
+		Short: "Pull Source OCI VNFS images",
+		Long:  "Pull Source OCI VNFS images ",
+		RunE:  CobraRunE,
+		Args:  cobra.MinimumNArgs(1),
+	}
+	SetForce  bool
+	SetUpdate bool
+	SetBuild  bool
+)
+
+func init() {
+	baseCmd.PersistentFlags().BoolVarP(&SetForce, "force", "f", false, "Force overwrite of an existing container")
+	baseCmd.PersistentFlags().BoolVarP(&SetUpdate, "update", "u", false, "Update and overwrite an existing container")
+	baseCmd.PersistentFlags().BoolVarP(&SetBuild, "build", "b", false, "Build container when after pulling")
+
+}
+
+// GetRootCommand returns the root cobra.Command for the application.
+func GetCommand() *cobra.Command {
+	return baseCmd
+}
