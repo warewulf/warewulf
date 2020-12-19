2ac2625b299099dfab2d5ef9d3f9544cb877262f
Gregory Kurtzer <gmkurtzer@gmail.com>
2020-12-19 00:40:05 -0800

    Add basic NFS configure command template

--- internal/app/wwctl/configure/nfs/root.go
+++ internal/app/wwctl/configure/nfs/root.go
@@ -1,1 +1,22 @@
 package nfs
+
+import "github.com/spf13/cobra"
+
+var (
+	baseCmd = &cobra.Command{
+		Use:   "nfs",
+		Short: "NFS configuration",
+		Long:  "NFS Config",
+		RunE:  CobraRunE,
+	}
+	SetShow bool
+)
+
+func init() {
+	baseCmd.PersistentFlags().BoolVarP(&SetShow, "show", "s", false, "Show configuration (don't update)")
+}
+
+// GetRootCommand returns the root cobra.Command for the application.
+func GetCommand() *cobra.Command {
+	return baseCmd
+}
