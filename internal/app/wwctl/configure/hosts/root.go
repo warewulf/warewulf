21d2a7ec00b1ad4e97d5b9d0b45cdaa36f3deee9
Gregory Kurtzer <gmkurtzer@gmail.com>
2020-12-19 00:39:19 -0800

    Add ability to configure host file on the controller node

--- internal/app/wwctl/configure/hosts/root.go
+++ internal/app/wwctl/configure/hosts/root.go
@@ -1,1 +1,22 @@
 package hosts
+
+import "github.com/spf13/cobra"
+
+var (
+	baseCmd = &cobra.Command{
+		Use:   "hosts",
+		Short: "NFS configuration",
+		Long:  "NFS Config",
+		RunE:  CobraRunE,
+	}
+	SetShow bool
+)
+
+func init() {
+	baseCmd.PersistentFlags().BoolVarP(&SetShow, "show", "s", false, "Show configuration (don't update)")
+}
+
+// GetRootCommand returns the root cobra.Command for the application.
+func GetCommand() *cobra.Command {
+	return baseCmd
+}
