16db64f9dce3af61f221377f96c8378833be5a07
Niko Kivel <niko.kivel@psi.ch>
2021-05-14 15:08:58 +0200

    removed group and controller, added ClusterName

--- internal/app/wwctl/node/add/main.go
+++ internal/app/wwctl/node/add/main.go
@@ -24,6 +24,16 @@ func CobraRunE(cmd *cobra.Command, args []string) error {
 		}
 		fmt.Printf("Added node: %s\n", a)
 
+	        if SetClusterName != "" {
+                        wwlog.Printf(wwlog.VERBOSE, "Node: %s, Setting cluster name to: %s\n", n.Id.Get(), SetClusterName)
+                        n.ClusterName.Set(SetClusterName)
+			err := nodeDB.NodeUpdate(n)
+			if err != nil {
+                                wwlog.Printf(wwlog.ERROR, "%s\n", err)
+                                os.Exit(1)
+                        }
+                }
+
 		if SetIpaddr != "" {
 			if SetNetDev == "" {
 				wwlog.Printf(wwlog.ERROR, "You must include the '--netdev' option\n")
