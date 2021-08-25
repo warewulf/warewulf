a9fcf5e74983546a366e50d908b95db98e322947
Michael L. Young <elgueromexicano@gmail.com>
2021-08-25 08:23:22 -0400

    Add IPMI Power Commands, Soft And Reset

---
+++ internal/app/wwctl/power/soft/power.go
@@ -0,0 +1,95 @@
+package powersoft
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hpcng/warewulf/internal/pkg/batch"
+	"github.com/hpcng/warewulf/internal/pkg/node"
+	"github.com/hpcng/warewulf/internal/pkg/power"
+	"github.com/hpcng/warewulf/internal/pkg/wwlog"
+	"github.com/spf13/cobra"
+)
+
+func CobraRunE(cmd *cobra.Command, args []string) error {
+	var returnErr error = nil
+
+	nodeDB, err := node.New()
+	if err != nil {
+		wwlog.Printf(wwlog.ERROR, "Could not open node configuration: %s\n", err)
+		os.Exit(1)
+	}
+
+	nodes, err := nodeDB.FindAllNodes()
+	if err != nil {
+		wwlog.Printf(wwlog.ERROR, "Cloud not get nodeList: %s\n", err)
+		os.Exit(1)
+	}
+
+	if len(args) > 0 {
+		nodes = node.FilterByName(nodes, args)
+	} else {
+		cmd.Usage()
+		os.Exit(1)
+	}
+
+	if len(nodes) == 0 {
+		fmt.Printf("No nodes found\n")
+		os.Exit(1)
+	}
+
+	batchpool := batch.New(50)
+	jobcount := len(nodes)
+	results := make(chan power.IPMI, jobcount)
+
+	for _, node := range nodes {
+
+		if node.IpmiIpaddr.Get() == "" {
+			wwlog.Printf(wwlog.ERROR, "%s: No IPMI IP address\n", node.Id.Get())
+			continue
+		}
+		var ipmiInterface = "lan"
+		if node.IpmiInterface.Get() != "" {
+			ipmiInterface = node.IpmiInterface.Get()
+		}
+		var ipmiPort = "623"
+		if node.IpmiPort.Get() != "" {
+			ipmiPort = node.IpmiPort.Get()
+		}
+		ipmiCmd := power.IPMI{
+			NodeName:  node.Id.Get(),
+			HostName:  node.IpmiIpaddr.Get(),
+			Port:      ipmiPort,
+			User:      node.IpmiUserName.Get(),
+			Password:  node.IpmiPassword.Get(),
+			Interface: ipmiInterface,
+			AuthType:  "MD5",
+		}
+
+		batchpool.Submit(func() {
+			ipmiCmd.PowerSoft()
+			results <- ipmiCmd
+		})
+
+	}
+
+	batchpool.Run()
+
+	close(results)
+
+	for result := range results {
+
+		out, err := result.Result()
+
+		if err != nil {
+			wwlog.Printf(wwlog.ERROR, "%s: %s\n", result.NodeName, out)
+			returnErr = err
+			continue
+		}
+
+		fmt.Printf("%s: %s\n", result.NodeName, out)
+
+	}
+
+	return returnErr
+}
--- internal/pkg/power/power.go
+++ internal/pkg/power/power.go
@@ -14,6 +14,18 @@ type PowerOffInterface interface {
 	PowerOff() (result string, err error)
 }
 
+type PowerResetInterface interface {
+	PowerReset() (result string, err error)
+}
+
+type PowerSoftInterface interface {
+	PowerSoft() (result string, err error)
+}
+
+type PowerCycleInterface interface {
+	PowerCycle() (result string, err error)
+}
+
 type PowerStatusInterface interface {
 	PowerStatus() (result string, err error)
 }
