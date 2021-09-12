e679fa24fac7af458465f00738e9fb2c2b653fe7
Gregory Kurtzer <gmkurtzer@gmail.com>
2021-09-11 21:23:30 -0700

    Cleanup container list command

--- internal/app/wwctl/container/list/main.go
+++ internal/app/wwctl/container/list/main.go
@@ -27,7 +27,7 @@ func CobraRunE(cmd *cobra.Command, args []string) error {
 		nodemap[n.ContainerName.Get()]++
 	}
 
-	fmt.Printf("%-35s %-6s %-6s\n", "VNFS NAME", "BUILT", "NODES")
+	fmt.Printf("%-35s %-6s %-6s\n", "CONTAINER NAME", "BUILT", "NODES")
 	for _, source := range sources {
 		image := container.ImageFile(source)
 
@@ -37,54 +37,5 @@ func CobraRunE(cmd *cobra.Command, args []string) error {
 		fmt.Printf("%-35s %-6t %-6d\n", source, util.IsFile(image), nodemap[source])
 
 	}
-
-	/*
-		nconfig, _ := node.New()
-		nodes, _ := nconfig.FindAllNodes()
-		nodemap := make(map[string]int)
-
-		for _, n := range nodes {
-			nodemap[n.Vnfs.Get()]++
-		}
-
-		images, _ := ioutil.ReadDir(config.VnfsImageParentDir())
-
-		fmt.Printf("%-38s %-16s %s\n", "VNFS Name", "VNFS SIZE(k)", "NODES")
-		fmt.Println(strings.Repeat("=", 80))
-
-		for _, file := range images {
-			v, err := container.Load(file.Name())
-			if err == nil {
-				var vnfs_size int64
-				if util.IsFile(config.VnfsImage(file.Name())) {
-					s, _ := os.Stat(config.VnfsImage(file.Name()))
-					vnfs_size = s.Size() / 1024
-				}
-
-				if nodemap[v.Source] > 0 {
-					fmt.Printf("%-38s %-16d %d\n", v.Source, vnfs_size, nodemap[v.Source])
-				} else {
-					fmt.Printf("%-38s %-16d %d\n", v.Source, vnfs_size, 0)
-				}
-
-			}
-			continue
-
-			if util.IsDir(path.Join(config.VnfsImageParentDir(), file.Name())) {
-				var vnfs_size int64
-				if util.IsFile(config.VnfsImage(file.Name())) {
-					s, _ := os.Stat(config.VnfsImage(file.Name()))
-					vnfs_size = s.Size() / 1024
-				}
-
-				if nodemap[file.Name()] > 0 {
-					fmt.Printf("%-38s %-16d %d\n", file.Name(), vnfs_size, nodemap[file.Name()])
-				} else {
-					fmt.Printf("%-38s %-16d %d\n", file.Name(), vnfs_size, 0)
-				}
-			}
-		}
-	*/
-
 	return nil
 }
