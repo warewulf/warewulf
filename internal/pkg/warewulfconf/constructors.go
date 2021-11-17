c06717d57914ffe668b4b1e728fbca2b33cfa779
carterpeel <carterpeel@gmail.com>
2021-11-16 19:51:01 -0500

    Revert "Added IP match check"
    This reverts commit 7a13f0d87f00ce745f40c143bc9c397d13c8a4ad.

--- internal/pkg/warewulfconf/constructors.go
+++ internal/pkg/warewulfconf/constructors.go
@@ -22,44 +22,23 @@ func New() (ret ControllerConf, err error) {
 			return ret, err
 		}
 
-		wwlog.Printf(wwlog.DEBUG, "Unmarshalling the Warewulf configuration\n")
+		wwlog.Printf(wwlog.DEBUG, "Unmarshaling the Warewulf configuration\n")
 		if err = yaml.Unmarshal(data, &ret); err != nil {
 		    return ret, err
         }
 
-		if ret.Ipaddr == "" && ret.HostBinding == "" {
+		if ret.Ipaddr == "" {
 			wwlog.Printf(wwlog.WARN, "IP address is not configured in warewulfd.conf\n")
 		}
-		if ret.Netmask == "" && ret.HostBinding == "" {
+		if ret.Netmask == "" {
 			wwlog.Printf(wwlog.WARN, "Netmask is not configured in warewulfd.conf\n")
 		}
 
 		if ret.Network == "" {
-		    var deviceIP string
-		    if ret.HostBinding != "" {
-                if deviceIP, err = util.HostnameToV4(ret.HostBinding); err != nil {
-                    wwlog.Printf(wwlog.WARN, "Error converting HostBinding to ipv4: %v\n", err)
+            if ip := net.ParseIP(ret.Ipaddr); ip == nil {
+                if ret.Ipaddr, err = util.HostnameToV4(ret.Ipaddr); err != nil {
                     return ret, err
                 }
-                if len(ret.Ipaddr)+len(ret.Netmask) > 0 {
-                    if !net.ParseIP(deviceIP).Equal(net.ParseIP(ret.Ipaddr)) {
-                        wwlog.Printf(wwlog.WARN, "IP resolved from device hostname (%s) does not match '%s'\n", deviceIP, ret.Ipaddr)
-                        return ret, fmt.Errorf("IP resolved from device hostname (%s) does not match '%s'", deviceIP, ret.Ipaddr)
-                    }
-                } else {
-                    ret.Ipaddr = net.ParseIP(deviceIP).String()
-                }
-            }
-
-            Addresses, err := util.GetLocalAddresses()
-            if err != nil {
-                wwlog.Printf(wwlog.WARN, "Error getting local addresses: %v\n", err)
-                return ret, err
-            }
-
-            if _, ok := Addresses[ret.Ipaddr]; !ok {
-                wwlog.Printf(wwlog.WARN, "Specified IP could not be found locally\n")
-                return ret, fmt.Errorf("specified IP could not be found locally")
             }
 
 			mask := net.IPMask(net.ParseIP(ret.Netmask).To4())
