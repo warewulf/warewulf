b09c9a8b98302b0230b6c7a3c8559444ff48ad7a
Gregory Kurtzer <gmkurtzer@gmail.com>
2022-01-15 04:34:03 +0000

    Fix so Warewulf can build and lint without make config

---
+++ internal/pkg/buildconfig/defaults.go
@@ -0,0 +1,16 @@
+package buildconfig
+
+var (
+	BINDIR         string = "UNDEF"
+	SYSCONFDIR     string = "UNDEF"
+	LOCALSTATEDIR  string = "UNDEF"
+	SRVDIR         string = "UNDEF"
+	TFTPDIR        string = "UNDEF"
+	FIREWALLDDIR   string = "UNDEF"
+	SYSTEMDDIR     string = "UNDEF"
+	WWOVERLAYDIR   string = "UNDEF"
+	WWCHROOTDIR    string = "UNDEF"
+	WWPROVISIONDIR string = "UNDEF"
+	VERSION        string = "UNDEF"
+	RELEASE        string = "UNDEF"
+)
