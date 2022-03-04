f90f0452d4d3d81f9d55a8466f11964057767794
Christian Goll <cgoll@suse.de>
2022-03-04 11:25:26 +0100

    added grpc call for version * this also implictly declarates the ApiPrefix and ApiVersion * grpc call is just internal, not through wwapid, yet

--- internal/app/wwctl/version/main.go
+++ internal/app/wwctl/version/main.go
@@ -3,13 +3,16 @@ package version
 import (
 	"fmt"
 
+	"github.com/hpcng/warewulf/internal/pkg/api/routes/wwapiv1"
 	"github.com/hpcng/warewulf/internal/pkg/version"
 	"github.com/spf13/cobra"
 )
 
 func CobraRunE(cmd *cobra.Command, args []string) error {
 
-	fmt.Println("Version:\t", version.GetVersion())
+	fmt.Println("wwctl version:\t", version.GetVersion())
 
+	var wwVersionResponse *wwapiv1.VersionResponse = version.Version()
+	fmt.Println("rpc version:", wwVersionResponse.String())
 	return nil
 }
