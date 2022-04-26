0a9e4b89a16d12bfda03766ab6129e4615d41a4f
Christian Goll <cgoll@suse.de>
2022-04-26 11:30:04 +0200

    fixup format

--- internal/app/wwctl/version/main.go
+++ internal/app/wwctl/version/main.go
@@ -10,18 +10,20 @@ import (
 
 func CobraRunE(cmd *cobra.Command, args []string) error {
 
-	fmt.Println("VERSION:\t", version.GetVersion())
-	fmt.Println("BINDIR:\t", buildconfig.BINDIR())
-	fmt.Println("DATADIR:\t", buildconfig.DATADIR())
-	fmt.Println("SYSCONFDIR:\t", buildconfig.SYSCONFDIR())
-	fmt.Println("LOCALSTATEDIR:\t", buildconfig.LOCALSTATEDIR())
-	fmt.Println("SRVDIR:\t", buildconfig.SRVDIR())
-	fmt.Println("TFTPDIR:\t", buildconfig.TFTPDIR())
-	fmt.Println("SYSTEMDDIR:\t", buildconfig.SYSTEMDDIR())
-	fmt.Println("WWOVERLAYDIR:\t", buildconfig.WWOVERLAYDIR())
-	fmt.Println("WWCHROOTDIR:\t", buildconfig.WWCHROOTDIR())
-	fmt.Println("WWPROVISIONDIR:\t", buildconfig.WWPROVISIONDIR())
-	fmt.Println("WWCLIENTDIR:\t", buildconfig.WWCLIENTDIR())
+	fmt.Printf("%-20s %-18s\n", "VERSION:", version.GetVersion())
+	fmt.Printf("%-20s %-18s\n", "BINDIR:", buildconfig.BINDIR())
+	fmt.Printf("%-20s %-18s\n", "DATADIR:", buildconfig.DATADIR())
+	fmt.Printf("%-20s %-18s\n", "SYSCONFDIR:", buildconfig.SYSCONFDIR())
+	fmt.Printf("%-20s %-18s\n", "LOCALSTATEDIR:", buildconfig.LOCALSTATEDIR())
+	fmt.Printf("%-20s %-18s\n", "SRVDIR:", buildconfig.SRVDIR())
+	fmt.Printf("%-20s %-18s\n", "TFTPDIR:", buildconfig.TFTPDIR())
+	fmt.Printf("%-20s %-18s\n", "SYSTEMDDIR:", buildconfig.SYSTEMDDIR())
+	fmt.Printf("%-20s %-18s\n", "WWOVERLAYDIR:", buildconfig.WWOVERLAYDIR())
+	fmt.Printf("%-20s %-18s\n", "WWCHROOTDIR:", buildconfig.WWCHROOTDIR())
+	fmt.Printf("%-20s %-18s\n", "WWPROVISIONDIR:", buildconfig.WWPROVISIONDIR())
+	fmt.Printf("%-20s %-18s\n", "BASEVERSION:", buildconfig.VERSION())
+	fmt.Printf("%-20s %-18s\n", "RELEASE:", buildconfig.RELEASE())
+	fmt.Printf("%-20s %-18s\n", "WWCLIENTDIR:", buildconfig.WWCLIENTDIR())
 
 	return nil
 }
