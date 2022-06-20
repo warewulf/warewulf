a79503eddd51ee515ca4702048b71587d007a600
Christian Goll <cgoll@suse.de>
2022-06-20 14:46:00 +0200

    Added right mock build

--- ci/mockbuild.sh
+++ ci/mockbuild.sh
@@ -15,3 +15,6 @@ mv /var/lib/mock/rocky+epel-8-x86_64/result/warewulf-${VERSION}-${RELEASE}.el8.x
 
 mock -r centos+epel-7-x86_64 --rebuild --spec=warewulf-${VERSION}/warewulf.spec --sources=.
 mv /var/lib/mock/centos+epel-7-x86_64/result/warewulf-${VERSION}-${RELEASE}.el7.x86_64.rpm .
+
+mock -r opensuse-leap-15.3-x86_64 --rebuild --spec=warewulf-${VERSION}/warewulf.spec --sources=.
+mv /var/lib/mock/opensuse-leap-15.3-x86_64/result/warewulf-${VERSION}-${RELEASE}.suse.lp153.x86_64.rpm .
