718064e67145b1decf7b128fcf497efa78a8c55f
Christian Goll <cgoll@suse.de>
2022-12-22 15:00:12 +0100

    add confirmation for `wwctl container delete`
    close #606
    
    Signed-off-by: Christian Goll <cgoll@suse.de>

--- internal/app/wwctl/container/delete/root.go
+++ internal/app/wwctl/container/delete/root.go
@@ -8,10 +8,10 @@ import (
 var (
 	baseCmd = &cobra.Command{
 		DisableFlagsInUseLine: true,
-		Use:   "delete [OPTIONS] CONTAINER [...]",
-		Short: "Delete an imported container",
-		Long:  "This command will delete CONTAINERs that have been imported into Warewulf.",
-		RunE:  CobraRunE,
+		Use:                   "delete [OPTIONS] CONTAINER [...]",
+		Short:                 "Delete an imported container",
+		Long:                  "This command will delete CONTAINERs that have been imported into Warewulf.",
+		RunE:                  CobraRunE,
 		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 			if len(args) != 0 {
 				return nil, cobra.ShellCompDirectiveNoFileComp
@@ -20,10 +20,11 @@ var (
 			return list, cobra.ShellCompDirectiveNoFileComp
 		},
 	}
+	SetYes bool
 )
 
 func init() {
-
+	baseCmd.PersistentFlags().BoolVarP(&SetYes, "yes", "y", false, "Set 'yes' to all questions asked")
 }
 
 // GetRootCommand returns the root cobra.Command for the application.
