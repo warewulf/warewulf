e3472f24da3d096af4ba12786bfde6fcdfda0bdd
Christian Goll <cgoll@suse.de>
2023-02-01 10:56:51 +0100

    correct behavior of profile add to yes flag

--- internal/app/wwctl/profile/add/main.go
+++ internal/app/wwctl/profile/add/main.go
@@ -38,10 +38,6 @@ func CobraRunE(cmd *cobra.Command, args []string) (err error) {
 	if !SetYes {
 		// The checks run twice in the prompt case.
 		// Avoiding putting in a blocking prompt in an API.
-		err = apiprofile.AddProfile(&set, false)
-		if err != nil {
-			return
-		}
 		_, _, err = apiprofile.ProfileSetParameterCheck(&set, false)
 		if err != nil {
 			return
@@ -52,5 +48,5 @@ func CobraRunE(cmd *cobra.Command, args []string) (err error) {
 			return
 		}
 	}
-	return apiprofile.ProfileSet(&set)
+	return apiprofile.AddProfile(&set, false)
 }
