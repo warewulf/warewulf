70361425ff0a5bc6bdaa167b2a542a81062093ff
Christian Goll <cgoll@suse.de>
2023-03-02 11:01:11 +0100

    generate man pages via sub command
    Signed-off-by: Christian Goll <cgoll@suse.de>

--- internal/app/wwctl/genconf/root.go
+++ internal/app/wwctl/genconf/root.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hpcng/warewulf/internal/app/wwctl/genconf/man"
 	"github.com/hpcng/warewulf/internal/app/wwctl/genconf/reference"
 	"github.com/hpcng/warewulf/internal/app/wwctl/genconf/warewulfconf"
+	"github.com/hpcng/warewulf/internal/app/wwctl/genconf/man"
 	"github.com/spf13/cobra"
 )
 
