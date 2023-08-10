a8a68e47adce41068ca34a50c1146a29d72700a1
Jonathon Anderson <janderson@ciq.com>
2023-08-10 01:33:02 -0600

    Change Warewulf version to 4.5.x
    Future releases from the development branch will be 4.5.x. Additional
    4.4.x versions would be tagged from the 4.4.x branch.
    Signed-off-by: Jonathon Anderson <janderson@ciq.com>

--- Variables.mk
+++ Variables.mk
@@ -10,7 +10,7 @@ VARLIST := OS
 # Project Information
 VARLIST += WAREWULF VERSION RELEASE
 WAREWULF ?= warewulf
-VERSION ?= 4.4.0
+VERSION ?= 4.5.x
 GIT_TAG := $(shell test -e .git && git log -1 --format="%h")
 
 ifdef GIT_TAG
