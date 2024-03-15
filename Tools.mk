d78af656def24e7da0058b2ab5509a7bebe3a606
Xu Yang <xyang@ciq.com>
2024-03-15 02:35:49 -0600

    upgrade minimum golang version
    Signed-off-by: Xu Yang <xyang@ciq.com>

--- Tools.mk
+++ Tools.mk
@@ -5,7 +5,7 @@ GO_TOOLS_BIN := $(addprefix $(TOOLS_BIN)/, $(notdir $(GO_TOOLS)))
 GO_TOOLS_VENDOR := $(addprefix vendor/, $(GO_TOOLS))
 
 GOLANGCI_LINT := $(TOOLS_BIN)/golangci-lint
-GOLANGCI_LINT_VERSION := v1.53.2
+GOLANGCI_LINT_VERSION := v1.56.2
 
 PROTOC := $(TOOLS_BIN)/protoc
 PROTOC_GEN_GO := $(TOOLS_BIN)/protoc-gen-go
