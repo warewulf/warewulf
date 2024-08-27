8f02c84c1ad6e791a9948fdf952d328d5ed43c91
Tobias Poschwatta <poschwatta@zib.de>
2024-08-27 13:08:51 +0200

    cachecontrol option added
    - used for downloads via /images/
    - defaults to max-age=60
    
    Signed-off-by: Tobias Poschwatta <poschwatta@zib.de>

--- internal/pkg/warewulfd/images.go
+++ internal/pkg/warewulfd/images.go
@@ -5,6 +5,7 @@ import (
 	"path"
 	"strings"
 
+	warewulfconf "github.com/warewulf/warewulf/internal/pkg/config"
 	"github.com/warewulf/warewulf/internal/pkg/image"
 	"github.com/warewulf/warewulf/internal/pkg/util"
 	"github.com/warewulf/warewulf/internal/pkg/wwlog"
@@ -12,6 +13,7 @@ import (
 
 func ImagesSend(w http.ResponseWriter, req *http.Request) {
 	wwlog.Debug("Requested URL: %s", req.URL.String())
+	conf := warewulfconf.Get()
 
 	url := strings.Split(req.URL.Path, "?")[0]
 	path_parts := strings.Split(url, "/")
@@ -28,14 +30,19 @@ func ImagesSend(w http.ResponseWriter, req *http.Request) {
 	stage_file := path.Join(image.ImageParentDir(), image_name)
 	wwlog.Serv("stage_file '%s'", stage_file)
 
-	if util.IsFile(stage_file) {
-		err := sendFile(w, req, stage_file, "")
-		if err != nil {
-			wwlog.ErrorExc(err, "")
-			return
-		}
-	} else {
+	if !util.IsFile(stage_file) {
 		w.WriteHeader(http.StatusNotFound)
 		wwlog.Error("images: not found: %s", stage_file)
+		return
+	}
+
+	if conf.Warewulf.CacheControl != "" {
+		w.Header().Set("Cache-Control", conf.Warewulf.CacheControl)
+	}
+
+	err := sendFile(w, req, stage_file, "")
+	if err != nil {
+		wwlog.ErrorExc(err, "")
+		return
 	}
 }
