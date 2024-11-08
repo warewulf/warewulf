917f191abeb83a074a6161aced95b138f3a6605c
Christian Goll <cgoll@suse.com>
2024-11-08 15:59:10 +0100

    add support for ignition in configs
    Signed-off-by: Christian Goll <cgoll@suse.com>

--- dracut/modules.d/90wwinit/parse-wwinit.sh
+++ dracut/modules.d/90wwinit/parse-wwinit.sh
@@ -3,25 +3,27 @@
 
 [ -z "$root" ] && root=$(getarg root=)
 
-if [ "${root}" = "wwinit" ]
-then
-    info "root=${root}"
+if [ "${root}" = "wwinit" ] || [ "${root}" = "persistent" ] ; then
+    info "warewulf: root=${root}"
     export wwinit_uuid=$(dmidecode -s system-uuid)
     export wwinit_assetkey=$(dmidecode -s chassis-asset-tag)
     export wwinit_uri="$(getarg wwinit.uri)"
-
+    export wwinit_ignition="$(getarg wwinit.ignition)"
+    export wwinit_ip="$(getarg wwinit.ip)"
+    export wwinit_imagename="$(getarg wwinit.imagename)"
+    export wwinit_id=$(getarg wwinit.id)
     wwinit_tmpfs_size=$(getarg wwinit.tmpfs.size=)
-    if [ -n "$wwinit_tmpfs_size" ]
-    then
-        info "wwinit.tmpfs.size=${wwinit_tmpfs_size}"
+    if [ -n "$wwinit_tmpfs_size" ] ; then
+        info "warewulf: wwinit.tmpfs.size=${wwinit_tmpfs_size}"
         export wwinit_tmpfs_size_option="-o size=${wwinit_tmpfs_size}"
     fi
-
-    if [ -n "${wwinit_uri}" ]
-    then
-        info "Found root=${root} and a Warewulf server uri. Will boot from Warewulf."
+    if [ "${root}" = "persistent" ] ; then 
+        export wwinit_persistent="1"; info "warewulf: wwinit_persistent=$wwinit_persistent"
+    fi
+    if [ -n "${wwinit_uri}" ] ; then
+        info "warewulf: Found root=${root} and a Warewulf server uri. Will boot from Warewulf."
         rootok=1
     else
-        die "Found root=${root} but no Warewulf server uri. Cannot boot from Warewulf."
+        die "warewulf: Found root=${root} but no Warewulf server uri. Cannot boot from Warewulf."
     fi
 fi
