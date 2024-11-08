67f32c0c7eab2b871172d730db8912bf72fb700d
Christian Goll <cgoll@suse.com>
2024-11-08 15:59:10 +0100

    add support for ignition in configs
    Signed-off-by: Christian Goll <cgoll@suse.com>

--- dracut/modules.d/90wwinit/parse-wwinit.sh
+++ dracut/modules.d/90wwinit/parse-wwinit.sh
@@ -3,32 +3,36 @@
 
 [ -z "$root" ] && root=$(getarg root=)
 
-if [ "${root}" = "wwinit" ]
-then
-    info "root=${root}"
+if [ "${root}" = "tmpfs" ] || [ "${root}" = "initramfs" ] || [ "${root}" = "persistent" ] ; then
+    info "warewulf: root=${root}"
     uuid=$(dmidecode -s system-uuid)
     assetkey=$(dmidecode -s chassis-asset-tag | sed -E -e 's/(^ +| +$)//g' -e 's/^(Unknown|Not Specified)$//g' -e 's/ /_/g')
-    wwinit_uri="$(getarg wwinit.uri)?assetkey=${assetkey}&uuid=${uuid}"
-    export wwinit_container="${wwinit_uri}&stage=container&compress=gz"; info "wwinit_container=${wwinit_container}"
-    export wwinit_system="${wwinit_uri}&stage=system&compress=gz"; info "wwinit_system=${wwinit_system}"
-    export wwinit_runtime="${wwinit_uri}&stage=runtime&compress=gz"; info "wwinit_runtime=${wwinit_runtime}"
+    export wwinit_node="$(getarg wwinit.node)"; info "warewulf: wwinit_node=${wwinit_node}"
+    export wwinit_ip="$(getarg wwinit.ip)"; info "warewulf: wwinit_ip=${wwinit_ip}"
+    export wwinit_port="$(getarg wwinit.port)"; info "warewulf: wwinit_port=${wwinit_port}"
+    export wwinit_mac="$(getarg wwid)"; info "warewulf: wwinit_mac=${wwinit_mac}"
+    export wwinit_containername="$(getarg wwinit.containername)"; info "warewulf: wwinit_mac=${wwinit_containername}"
+    wwinit_uri="http://${wwinit_ip}:${wwinit_port}/provision/${wwinit_mac}?assetkey=${assetkey}&uuid=${uuid}"
+    export wwinit_container="${wwinit_uri}&stage=container&compress=gz"; info "warewulf: wwinit_container=${wwinit_container}"
+    export wwinit_system="${wwinit_uri}&stage=system&compress=gz"; info "warewulf: wwinit_system=${wwinit_system}"
+    export wwinit_runtime="${wwinit_uri}&stage=runtime&compress=gz"; info "warewulf: wwinit_runtime=${wwinit_runtime}"
     if [ -n "$(getarg wwinit.KernelOverride)" ]
     then
-        export wwinit_kmods="${wwinit_uri}&stage=kmods&compress=gz"; info "wwinit_kmods=${wwinit_kmods}"
+        export wwinit_kmods="${wwinit_uri}&stage=kmods&compress=gz"; info "warewulf: wwinit_kmods=${wwinit_kmods}"
     fi
-
     wwinit_tmpfs_size=$(getarg wwinit.tmpfs.size=)
     if [ -n "$wwinit_tmpfs_size" ]
     then
-        info "wwinit.tmpfs.size=${wwinit_tmpfs_size}"
+        info "warewulf: wwinit.tmpfs.size=${wwinit_tmpfs_size}"
         export wwinit_tmpfs_size_option="-o size=${wwinit_tmpfs_size}"
     fi
-
-    if [ -n "${wwinit_container}" ]
-    then
-        info "Found root=${root} and a Warewulf container image. Will boot from Warewulf."
+    if [ "${root}" = "persistent" ] ; then 
+        export wwinit_persistent="1"; info "warewulf: wwinit_persistent=$wwinit_persistent"
+    fi
+    if [ -n "${wwinit_container}" ] ; then
+        info "warewulf: Found root=${root} and a Warewulf container image. Will boot from Warewulf."
         rootok=1
     else
-        die "Found root=${root} but no container image. Cannot boot from Warewulf."
+        die "warewulf: Found root=${root} but no container image. Cannot boot from Warewulf."
     fi
 fi
