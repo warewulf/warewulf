6daca7ccb0a627b2a14b5076410a8cabe6239ebd
Christian Goll <cgoll@suse.com>
2024-11-20 17:00:34 +0100

    update rsync conf after container operations
    Signed-off-by: Christian Goll <cgoll@suse.com>

--- internal/pkg/configure/rsync.go
+++ internal/pkg/configure/rsync.go
@@ -14,40 +14,49 @@ import (
 
 func RSYNC() (err error) {
 	controller := warewulfconf.Get()
-	rsyncTemplate := path.Join(overlay.OverlaySourceDir("host"), controller.RSYNC.Conf+".ww")
-	if !(util.IsFile(rsyncTemplate)) {
-		return fmt.Errorf("'the overlay template '%s' does not exists in 'host' overlay", controller.RSYNC.Conf+".ww")
-	}
-	hostname, _ := os.Hostname()
-	tstruct, err := overlay.InitStruct(node.NewNode(hostname))
-	if err != nil {
-		return err
-	}
-	buffer, backupFile, writeFile, err := overlay.RenderTemplateFile(
-		rsyncTemplate,
-		tstruct)
-	if err != nil {
-		return
-	}
-	info, err := os.Stat(rsyncTemplate)
-	if err != nil {
-		return
-	}
-	if writeFile {
-		err = overlay.CarefulWriteBuffer(controller.RSYNC.Conf, buffer, backupFile, info.Mode())
+	if controller.RSYNC.Enabled() {
+		rsyncTemplate := path.Join(overlay.GetOverlay("host").Rootfs(), controller.RSYNC.Conf+".ww")
+		if !(util.IsFile(rsyncTemplate)) {
+			return fmt.Errorf("'the rsync overlay template '%s' does not exists in 'host' overlay", controller.RSYNC.Conf+".ww")
+		}
+		nodeDb, err := node.New()
 		if err != nil {
-			return fmt.Errorf("could not write file from template: %w", err)
+			return err
+		}
+		allNodes, err := nodeDb.FindAllNodes()
+		if err != nil {
+			return err
+		}
+		hostname, _ := os.Hostname()
+		tstruct, err := overlay.InitStruct("host", node.NewNode(hostname), allNodes)
+		if err != nil {
+			return err
+		}
+		buffer, backupFile, writeFile, err := overlay.RenderTemplateFile(
+			rsyncTemplate,
+			tstruct)
+		if err != nil {
+			return err
+		}
+		info, err := os.Stat(rsyncTemplate)
+		if err != nil {
+			return err
+		}
+		if writeFile {
+			err = overlay.CarefulWriteBuffer(controller.RSYNC.Conf, buffer, backupFile, info.Mode())
+			if err != nil {
+				return fmt.Errorf("could not write file from template: %w", err)
+			}
+		}
+		wwlog.Info("Enabling and restarting the rsyncd services")
+		if controller.RSYNC.SystemdName == "" {
+			return fmt.Errorf("no name for rsyncd service defined in warewulf.conf")
+		} else {
+			err := util.SystemdStart(controller.RSYNC.SystemdName)
+			if err != nil {
+				return fmt.Errorf("failed to start: %w", err)
+			}
 		}
 	}
-	wwlog.Info("Enabling and restarting the rsyncd services")
-	if controller.RSYNC.SystemdName == "" {
-		return fmt.Errorf("no name for rsyncd service defined in warewulf.conf")
-	} else {
-		err := util.SystemdStart(controller.RSYNC.SystemdName)
-		if err != nil {
-			return fmt.Errorf("failed to start: %w", err)
-		}
-	}
-	return
-
+	return nil
 }
