c03548c74ea610435ddeae1278acace633d496a0
Ian Kaufman <ikaufman@ucsd.edu>
2024-12-03 12:48:40 -0800

    Update netplan_test.go Fixing file name suffix

--- overlays/netplan/internal/netplan_test.go
+++ overlays/netplan/internal/netplan_test.go
@@ -14,7 +14,7 @@ func Test_netplanOverlay(t *testing.T) {
 	env := testenv.New(t)
 	defer env.RemoveAll(t)
 	env.ImportFile(t, "etc/warewulf/nodes.conf", "nodes.conf")
-	env.ImportFile(t, "var/lib/warewulf/overlays/netplan/rootfs/etc/netplan/01-netcfg.yaml.ww", "../rootfs/etc/netplan/01-netcfg.yaml")
+	env.ImportFile(t, "var/lib/warewulf/overlays/netplan/rootfs/etc/netplan/01-netcfg.yaml.ww", "../rootfs/etc/netplan/01-netcfg.yaml.ww")
 
 	tests := []struct {
 		name string
