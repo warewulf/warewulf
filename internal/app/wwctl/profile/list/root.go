60872bd42ed9e7b8bdb95e1c0ca78895ab3457a0
Christian Goll <cgoll@suse.com>
2025-02-05 21:48:07 +0100

    Add completion for profile list
    - Closes: #1696
    
    Signed-off-by: Christian Goll <cgoll@suse.com>

--- internal/app/wwctl/profile/list/root.go
+++ internal/app/wwctl/profile/list/root.go
@@ -1,6 +1,9 @@
 package list
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"github.com/warewulf/warewulf/internal/pkg/node"
+)
 
 type variables struct {
 	showAll  bool
@@ -18,6 +21,13 @@ func GetCommand() *cobra.Command {
 		Long:                  "This command will display configurations for PROFILE.",
 		RunE:                  CobraRunE(&vars),
 		Aliases:               []string{"ls"},
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			if len(args) != 0 {
+				return nil, cobra.ShellCompDirectiveNoFileComp
+			}
+			nodeDB, _ := node.New()
+			return nodeDB.ListAllProfiles(), cobra.ShellCompDirectiveNoFileComp
+		},
 	}
 	baseCmd.PersistentFlags().BoolVarP(&vars.showAll, "all", "a", false, "Show all profile configurations")
 	baseCmd.PersistentFlags().BoolVarP(&vars.showYaml, "yaml", "y", false, "Show profile configurations via yaml format")
