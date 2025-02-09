32b9696b71b2f62f9142e1aefe6de31c698eb1e1
Jonathon Anderson <janderson@ciq.com>
2025-02-09 08:38:04 -0700

    wwctl power args and completions
    Signed-off-by: Jonathon Anderson <janderson@ciq.com>

--- internal/app/wwctl/power/reset/root.go
+++ internal/app/wwctl/power/reset/root.go
@@ -2,7 +2,7 @@ package reset
 
 import (
 	"github.com/spf13/cobra"
-	"github.com/warewulf/warewulf/internal/pkg/node"
+	"github.com/warewulf/warewulf/internal/app/wwctl/completions"
 )
 
 type variables struct {
@@ -19,19 +19,8 @@ func GetCommand() *cobra.Command {
 		Short:                 "Issue a reset to node(s)",
 		Long:                  "This command will issue a reset to a set of nodes specified by PATTERN.",
 		RunE:                  CobraRunE(&vars),
-		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-			if len(args) != 0 {
-				return nil, cobra.ShellCompDirectiveNoFileComp
-			}
-
-			nodeDB, _ := node.New()
-			nodes, _ := nodeDB.FindAllNodes()
-			var node_names []string
-			for _, node := range nodes {
-				node_names = append(node_names, node.Id())
-			}
-			return node_names, cobra.ShellCompDirectiveNoFileComp
-		},
+		Args:                  cobra.MinimumNArgs(1),
+		ValidArgsFunction:     completions.Nodes(0), // no limit,
 	}
 	powerCmd.PersistentFlags().BoolVarP(&vars.Showcmd, "show", "s", false, "only show command which will be executed")
 	powerCmd.PersistentFlags().IntVar(&vars.Fanout, "fanout", 50, "how many command should be executed in parallel")
