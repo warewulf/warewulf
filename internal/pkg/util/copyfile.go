c7bda5e655a2a32e0bc80fee17a7251c07df8fea
Jonathon Anderson <janderson@ciq.com>
2025-03-21 23:37:03 -0600

    Add tests for overlay import --overwrite
    Signed-off-by: Jonathon Anderson <janderson@ciq.com>

--- internal/pkg/util/copyfile.go
+++ internal/pkg/util/copyfile.go
@@ -14,27 +14,27 @@ func CopyFile(src string, dst string) error {
 	// Open source file
 	srcFD, err := os.Open(src)
 	if err != nil {
-		wwlog.Error("Could not open source file %s: %s", src, err)
+		wwlog.Debug("Could not open source file %s: %s", src, err)
 		return err
 	}
 	defer srcFD.Close()
 
 	srcInfo, err := srcFD.Stat()
 	if err != nil {
-		wwlog.Error("Could not stat source file %s: %s", src, err)
+		wwlog.Debug("Could not stat source file %s: %s", src, err)
 		return err
 	}
 
 	dstFD, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE|os.O_TRUNC, srcInfo.Mode())
 	if err != nil {
-		wwlog.Error("Could not create destination file %s: %s", dst, err)
+		wwlog.Debug("Could not create destination file %s: %s", dst, err)
 		return err
 	}
 	defer dstFD.Close()
 
 	bytes, err := io.Copy(dstFD, srcFD)
 	if err != nil {
-		wwlog.Error("File copy from %s to %s failed.\n %s", src, dst, err)
+		wwlog.Debug("File copy from %s to %s failed.\n %s", src, dst, err)
 		return err
 	} else {
 		wwlog.Debug("Copied %d bytes from %s to %s.", bytes, src, dst)
@@ -42,7 +42,7 @@ func CopyFile(src string, dst string) error {
 
 	err = CopyUIDGID(src, dst)
 	if err != nil {
-		wwlog.Error("Ownership copy from %s to %s failed.\n %s", src, dst, err)
+		wwlog.Debug("Ownership copy from %s to %s failed.\n %s", src, dst, err)
 		return err
 	}
 	return nil
