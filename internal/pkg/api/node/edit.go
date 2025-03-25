ca1830d8e17e285f81d9c57861352335ff34a260
Rafael Lopez <rafael.lopez@smc.co>
2025-03-25 14:55:09 +1100

    Rename value in loop
    Rename value from 'node' to 'nodeData' so it doesn't
    coincide with the package import name.
    
    Signed-off-by: Rafael Lopez <raflopez1@gmail.com>

--- internal/pkg/api/node/edit.go
+++ internal/pkg/api/node/edit.go
@@ -21,14 +21,14 @@ func NodeAddFromYaml(nodeList *wwapiv1.NodeYaml) (err error) {
 	if err != nil {
 		return fmt.Errorf("could not unmarshal Yaml: %w", err)
 	}
-	for nodeName, node := range nodeMap {
+	for nodeName, nodeData := range nodeMap {
 		if _, err = nodeDB.GetNodeOnly(nodeName); err == node.ErrNotFound {
 			_, err = nodeDB.AddNode(nodeName)
 			if err != nil {
 				return fmt.Errorf("couldn't add new node: %w", err)
 			}
 		}
-		err = nodeDB.SetNode(nodeName, *node)
+		err = nodeDB.SetNode(nodeName, *nodeData)
 		if err != nil {
 			return fmt.Errorf("couldn't set node: %w", err)
 		}
