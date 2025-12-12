38b5e2eb831bb50592972bfd4059d37cc638d653
Christian Goll <cgoll@suse.com>
2025-12-12 09:41:08 +0100

    display data of the eventlog

--- internal/app/wwctl/node/tpm/main.go
+++ internal/app/wwctl/node/tpm/main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"sort"
 
 	"github.com/google/go-attestation/attest"
 	"github.com/spf13/cobra"
@@ -76,13 +75,10 @@ func displayEventLog(b64Log string) error {
 	}
 
 	events := el.Events(attest.HashSHA256)
-	sort.Slice(events, func(i, j int) bool {
-		return events[i].Index < events[j].Index
-	})
 
 	fmt.Println("TPM Event Log (SHA256):")
 	for _, event := range events {
-		fmt.Printf("PCR[%d] Type=%s Digest=%x\n", event.Index, event.Type, event.Digest)
+		fmt.Printf("PCR[%d] Type=%s Digest=%x Data=%s\n", event.Index, event.Type, event.Digest, tpm.FormatEventData(event))
 	}
 	return nil
 }
