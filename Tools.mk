cea25f5a879f0b98d63901b3afbbe31c407c1f70
Jonathon Anderson <janderson@ciq.com>
2026-03-10 20:03:08 -0600

    Update staticcheck for golang v1.25 compatibility
    Signed-off-by: Jonathon Anderson <janderson@ciq.com>

--- Tools.mk
+++ Tools.mk
@@ -59,7 +59,7 @@ $(GOLANG_LICENSES):
 	GOBIN="$(PWD)/$(TOOLS_BIN)" go install github.com/google/go-licenses@v1.6.0
 
 $(GOLANG_STATICCHECK):
-	GOBIN="$(PWD)/$(TOOLS_BIN)" go install honnef.co/go/tools/cmd/staticcheck@v0.5.1
+	GOBIN="$(PWD)/$(TOOLS_BIN)" go install honnef.co/go/tools/cmd/staticcheck@v0.7.0
 
 .PHONY: cleantools
 cleantools:
